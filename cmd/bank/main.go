4c28e92b349b1748946dfc0cd6734d1aa1be8036
Student <fhgf9048@gmail.com>
2021-10-31 19:51:09 +0500

    cmd добавлен

---
+++ cmd/bank/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	// "bank/pkg/bank/card"
+	"bank/pkg/bank/types"
+	"fmt"
+)
+
+func main() {
+	cards := []types.Card{
+		{
+			Balance: 10_000_00,
+			Active: true,
+		},
+		{
+			Balance: 10_000_00,
+			Active: true,
+		},
+		{
+			Balance: 10_000_00,
+			Active: true,
+		},
+	}
+
+	fmt.Println(cards)
+}
+
+
+
+// PAGE 54
